637db7ecd7aced6fbf5ea47a21557d5641a2730f
KatieGott <Katie.Gott@target.com>
2022-04-08 10:09:04 -0500

    first commit

---
+++ create.sql
@@ -0,0 +1,9 @@
+-- Postgres
+-- systems components are objects
+-- Db is a top level object
+CREATE DATABASE db_example;
+
+-- db names must start with a letter or _, no numbers
+-- tables are where data actually lives
+-- varaibles number of rows
+-- fixed number of columns (can be altered later)
